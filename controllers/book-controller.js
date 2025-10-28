b8b7128d65755561e269d55e9b0a513c5a47cfcb
jailcomfranssa <jail_son_0@hotmail.com>
2025-10-28 16:09:24 -0300

    criando controlle add book

--- controllers/book-controller.js
+++ controllers/book-controller.js
@@ -1,8 +1,24 @@
-
+const Book = require("../models/book");
 
 const getAllBooks = async (req, res) => {};
 const getSingleBookById = async (req, res) => {};
-const addNewBooks = async (req, res) => {};
+const addNewBooks = async (req, res) => {
+    try{
+        const newBookFormData = req.body;
+        const newlyCreatedBook = await Book.create(newBookFormData);
+        if (newlyCreatedBook) {
+            console.log("✅ New book added successfully:", newlyCreatedBook);
+            res.status(201).json({
+                success: true,
+                message: "New book added successfully",
+                book: newlyCreatedBook
+            });
+        }
+
+    }catch(error){
+        console.error("❌ Error adding new book:", error);
+    }
+};
 const updateBooks = async (req, res) => {};
 const deleteBooks = async (req, res) => {};
 
