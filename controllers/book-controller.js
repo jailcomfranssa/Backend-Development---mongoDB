e4d61079b6b56eeb262043d5cebb91cfb5920bb5
jailcomfranssa <jail_son_0@hotmail.com>
2025-10-28 15:35:45 -0300

    add book controller em routes

---
+++ controllers/book-controller.js
@@ -0,0 +1,15 @@
+
+
+const getAllBooks = async (req, res) => {};
+const getSingleBookById = async (req, res) => {};
+const addNewBooks = async (req, res) => {};
+const updateBooks = async (req, res) => {};
+const deleteBooks = async (req, res) => {};
+
+module.exports = {
+    getAllBooks,
+    getSingleBookById,
+    addNewBooks,
+    updateBooks,
+    deleteBooks,
+};
--- routes/book-routes.js
+++ routes/book-routes.js
@@ -1,12 +1,12 @@
 const express = require('express');
-
+const {getAllBooks, getSingleBookById, addNewBooks, updateBooks, deleteBooks} = require('../controllers/book-controller');
 const router = express.Router();
 
 //  all books routes will be defined here
-router.get('/books');
-router.get('/books/:id');
-router.post('/books');
-router.put('/books/:id');
-router.delete('/books/:id');
+router.get('/books', getAllBooks);
+router.get('/books/:id', getSingleBookById);
+router.post('/books', addNewBooks);
+router.put('/books/:id', updateBooks);
+router.delete('/books/:id', deleteBooks);
 
 module.exports = router;
