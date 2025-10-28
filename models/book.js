a7c798408daa2b0694c110b7dc43862cb91bf7bf
jailcomfranssa <jail_son_0@hotmail.com>
2025-10-28 15:13:17 -0300

    criando model de book

---
+++ models/book.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const BookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, "Book title is required"],
+        trim: true,
+        maxLength: [150, "Title cannot exceed 150 characters"],
+    },
+    author :{
+        type: String,
+        required: [true, "Author name is required"],
+        trim: true,
+    },
+    year :{
+        type: Number,
+        required: [true, "Publication year is required"],
+        min: [1000, "Year must be after 1000"],
+        max: [new Date().getFullYear(), "Year cannot be in the future"],
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+module.exports = mongoose.model("Book", BookSchema);
