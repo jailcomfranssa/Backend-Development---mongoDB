5d996f50a762eb33a9a316f7e69726cbf008aecb
jailcomfranssa <jail_son_0@hotmail.com>
2025-10-28 17:51:04 -0300

    criando função listar todos books

--- controllers/book-controller.js
+++ controllers/book-controller.js
@@ -1,7 +1,34 @@
 const Book = require("../models/book");
 const log = require("../utils/logger");
 
-const getAllBooks = async (req, res) => {};
+const getAllBooks = async (req, res) => {
+    try {
+        const allBooks = await Book.find({});
+        if(allBooks?.length > 0){
+            log.info("Fetched all books successfully:", allBooks);
+            res.status(200).json({
+                success: true,
+                message: "Fetched all books successfully",
+                books: allBooks
+            })
+
+        }else{
+            log.warn("No books found in the database.");
+            res.status(404).json({
+                success: false,
+                message: "No books found in the database."
+            });
+        }
+
+    } catch (error) {
+        log.error("Error fetching all books:", error);
+        res.status(500).json({
+            success: false,
+            message: "An error occurred while fetching books.",
+            error: error.message
+        });
+    }
+};
 const getSingleBookById = async (req, res) => {};
 const addNewBooks = async (req, res) => {
     try{
